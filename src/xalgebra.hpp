ec0f65520766f9e32c224b430654955aa94bf926
Taras Kolomatski <taras.kolomatski@uwaterloo.ca>
2019-07-14 18:56:20 -0400

    Added margins

--- src/xalgebra.hpp
+++ src/xalgebra.hpp
@@ -2,23 +2,33 @@
 #include "xtensor/xfixed.hpp"
 #include "xtensor/xrandom.hpp"
 #include "xtensor/xio.hpp"
+#include "xtensor/xview.hpp"
+#include "xtensor/xarray.hpp"
 
 using std::get;
+using namespace xt;
+
+
+//Compile tyme info via errors
+//Usage: TD<declype(var)> info;
+template<typename T>
+struct TD;
 
 template<typename T, size_t L, size_t W, size_t H>
 class xfield{
-  using field_t = xfield<T, L, W, H>;
-  using shape_t = xt::xshape<3, L, W, H>;
-  using storage_t = xt::xtensor_fixed<T, shape_t>;
+  using self_t = xfield<T, L, W, H>;
+  using shape_t = xshape<3, L, W, H>;
+  using storage_shape_t = xshape<3, L+4, W+4, H+4>;
+  using storage_t = xtensor_fixed<T, storage_shape_t>;
 
 public:
-
   void fillTG();
 
 private:
-  friend std::ostream & operator<<(std::ostream &os, const field_t& arg)
+  friend std::ostream & operator<<(std::ostream &os, const self_t& arg)
   {
-    return os << arg.data;
+    auto data_view = view(arg.data, all(), range(2, L + 2), range(2, W + 2), range(2, H + 2));
+    return os << data_view;
   }
 
   storage_t data;
@@ -28,15 +38,17 @@ template<typename T, size_t L, size_t W, size_t H>
 void xfield<T, L, W, H>::fillTG(){
   static const T pi = acos(-1);
 
-  auto x_space = xt::linspace<T>(pi / L, 2 * pi - pi / L, L);
-  auto y_space = xt::linspace<T>(pi / W, 2 * pi - pi / W, W);
-  auto z_space = xt::linspace<T>(pi / H, 2 * pi - pi / H, H);
+  auto x_space = linspace<T>(pi / L, 2 * pi - pi / L, L);
+  auto y_space = linspace<T>(pi / W, 2 * pi - pi / W, W);
+  auto z_space = linspace<T>(pi / H, 2 * pi - pi / H, H);
 
-  auto mesh = xt::meshgrid(z_space, y_space, x_space);
+  auto mesh = meshgrid(z_space, y_space, x_space);
 
-  auto X = xt::cos(get<2>(mesh)) * xt::sin(get<1>(mesh)) * xt::sin(get<0>(mesh));
-  auto Y = -0.5 * xt::cos(get<1>(mesh)) * xt::sin(get<0>(mesh)) * xt::sin(get<2>(mesh));
-  auto Z = -0.5 * xt::cos(get<0>(mesh)) * xt::sin(get<2>(mesh)) * xt::sin(get<1>(mesh));
+  auto X = cos(get<2>(mesh)) * sin(get<1>(mesh)) * sin(get<0>(mesh));
+  auto Y = -0.5 * cos(get<1>(mesh)) * sin(get<0>(mesh)) * sin(get<2>(mesh));
+  auto Z = -0.5 * cos(get<0>(mesh)) * sin(get<2>(mesh)) * sin(get<1>(mesh));
 
-  data = xt::stack(xt::xtuple(X, Y, Z));
+  auto data_view = view(data, all(), range(2, L + 2), range(2, W + 2), range(2, H + 2));
+
+  data_view = stack(xtuple(X, Y, Z));
 }
